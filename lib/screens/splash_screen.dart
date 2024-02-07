8250c4727e6a71195b9693c4d60870893404f75b
Jeremiah Thuku <thuku8@gmail.com>
2024-02-07 17:29:58 +0300

    added the splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'package:clothing_e_commerce_app/screens/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _mockAsyncInit().then((_) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+      );
+    });
+  }
+
+  Future _mockAsyncInit() async {
+    await Future.delayed(const Duration(seconds: 2));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+            'assets/app/app_image.png',
+            height: 150,
+            width: 150,
+        ),
+      ),
+    );
+  }
+}
